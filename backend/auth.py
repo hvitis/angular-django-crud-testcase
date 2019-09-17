20ce08d1c4d74172c7dc30b93a22e01f8d7fd6ce
hvitis <adam.piskorek.pl@gmail.com>
2019-09-17 05:36:50 +0200

    Eliminating deprecated OAuth library

--- backend/auth.py
+++ backend/auth.py
@@ -1,5 +1,8 @@
 from django.conf import settings
-from oauth2client import client, crypt
+# not-deprecated library for token validation
+from google.oauth2 import id_token
+from google.auth.transport import requests
+
 from django.contrib.auth import get_user_model
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.authentication import (
@@ -17,28 +20,28 @@ class GoogleAuthentication(TokenAuthentication):
 
     def authenticate_credentials(self, token):
         User = get_user_model()
-
         _token = self.request.session.get('token')
         email = self.request.session.get('email')
 
+        # Using id_token with new not-deprecated library
         if token != _token:
             try:
-                idinfo = client.verify_id_token(
-                    token, settings.GOOGLE_CLIENT_ID)
-                auth_domains = ['accounts.google.com',
-                                'https://accounts.google.com']
-                if idinfo['iss'] not in auth_domains:
-                    raise crypt.AppIdentityError("Wrong issuer.")
-
+                # Specify the CLIENT_ID of the app that accesses the backend:
+                idinfo = id_token.verify_oauth2_token(token, requests.Request(), settings.GOOGLE_CLIENT_ID)
+                if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+                    raise ValueError('Wrong issuer.')
+                # ID token is valid. 
+                # Get the user's Google Account ID from the decoded token.
+                userid = idinfo['sub']
+                # Get the user's email from the decoded token.
                 email = idinfo['email']
-            except crypt.AppIdentityError:
+            except ValueError:
+                # Invalid token
                 raise AuthenticationFailed('Invalid token.')
-
         user, created = User.objects.get_or_create(email=email, username=email)
         self.request.session['token'] = token
         self.request.session['email'] = email
         return (user, token)
-
 
 class CustomSessionAuthentication(SessionAuthentication):
     def authenticate_header(self, request):
