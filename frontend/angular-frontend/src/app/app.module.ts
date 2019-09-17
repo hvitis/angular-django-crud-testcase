07bbbce6574d56a05ca79a5119aba3e2beee2f50
hvitis <adam.piskorek.pl@gmail.com>
2019-09-17 05:24:54 +0200

    Initializing OAuth, adding HttpModule on FrontEnd

--- frontend/angular-frontend/src/app/app.module.ts
+++ frontend/angular-frontend/src/app/app.module.ts
@@ -2,15 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SocialLoginModule, AuthServiceConfig } from "angularx-social-login";
+import { GoogleLoginProvider } from "angularx-social-login";
+import { HttpClientModule } from '@angular/common/http';
+
+let config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider("256846098397-b4nb3bdkodne82v08q2u0da873e1gh3g.apps.googleusercontent.com")
+  },
+]);
+
+export function provideConfig() {
+  return config;
+}
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocialLoginModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{
+    provide: AuthServiceConfig,
+    useFactory: provideConfig
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
