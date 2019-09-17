321f55c419c768d84c65243b5321652897fa9439
hvitis <adam.piskorek.pl@gmail.com>
2019-09-17 05:29:43 +0200

    Simple Buttons with functions to test Service&API

--- frontend/angular-frontend/src/app/app.component.ts
+++ frontend/angular-frontend/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from "angularx-social-login";
+import { GoogleLoginProvider } from "angularx-social-login";
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-frontend';
+ 
+  constructor(private authService: AuthService, private api : ApiService) { }
+
+
+  getUsers(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then( (userData) => {
+      this.api.getUsers(userData.authToken) ,
+      console.log(userData)
+
+    }).catch((error) => console.log(error))
+  }
+
+
+  registerUser(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then( (userData) => {
+      this.api.postUser(userData.idToken, userData.email) ,
+      console.log(userData)
+
+    }).catch((error) => console.log(error))
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }
 }
